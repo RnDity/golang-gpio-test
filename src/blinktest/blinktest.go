45e47d8a3ae14a0105d8577fc6370a3273524c24
Maciej Borzecki <maciej.borzecki@open-rnd.pl>
2016-01-15 13:08:56 +0100

    blinktest: comments

--- src/blinktest/blinktest.go
+++ src/blinktest/blinktest.go
@@ -33,7 +33,7 @@ import (
 
 const (
 	REVOLUTIONS = 100
-	REV_PROC = 100000 * REVOLUTIONS
+	REV_PROC    = 100000 * REVOLUTIONS
 
 	usage = "" +
 		`Usage of 'blinktest' command
@@ -83,10 +83,17 @@ func main() {
 	}
 
 	chains := []*chain.Chain{
-		chain.New(REV_PROC / 1000, 1000, leds[0]),
-		chain.New(REV_PROC / 10000, 10000, leds[1]),
-		chain.New(REV_PROC / 25000, 25000, leds[2]),
-		chain.New(REV_PROC / 100000, 100000, leds[3]),
+		// use this set of chains to toggle GPIOs all the time
+		// chain.New(REV_PROC/1000, 1000, leds[0]),
+		// chain.New(REV_PROC/10000, 10000, leds[1]),
+		// chain.New(REV_PROC/25000, 25000, leds[2]),
+		// chain.New(REV_PROC/100000, 100000, leds[3]),
+
+		// use this set to have the chains exit one by one
+		chain.New(REVOLUTIONS, 1000, leds[0]),
+		chain.New(REVOLUTIONS, 10000, leds[1]),
+		chain.New(REVOLUTIONS, 25000, leds[2]),
+		chain.New(REVOLUTIONS, 100000, leds[3]),
 	}
 
 	fmt.Println("spawning goroutines")
@@ -102,11 +109,13 @@ func main() {
 		ch.Start()
 	}
 
+	// wait for chains to complete
 	for _, ch := range chains {
 		ch.Wait()
 	}
 	fmt.Println("finished")
 
+	// cleanup LEDs
 	for _, l := range leds {
 		l.Close()
 	}
